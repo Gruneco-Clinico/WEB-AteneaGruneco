afd256156439d84d669d0628885330a016300191
Juan Pablo Restrepo <108776232+JPDevOpti@users.noreply.github.com>
2025-06-17 08:53:52 -0500

    correcion de errores en el archivo wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,12 +1,10 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Asegura que el directorio raíz del proyecto esté en el path
+sys.path.append('/opt/bitnami/Repositories/WEB-AteneaGruneco')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
